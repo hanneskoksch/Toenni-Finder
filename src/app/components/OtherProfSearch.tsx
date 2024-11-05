f0b8350dd7ba6e9b7361d4aac4ee1d5161f55a6b
Hannes Koksch <hannes.koksch@gmail.com>
2024-11-05 16:49:27 +0100

    feat: clear input button

--- src/app/components/OtherProfSearch.tsx
+++ src/app/components/OtherProfSearch.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { XIcon } from "lucide-react";
 import { useState } from "react";
 
 interface OtherProfSearchProps {
@@ -25,9 +26,25 @@ function OtherProfSearch({ onSearch }: OtherProfSearchProps) {
               name="profName"
               type="text"
               placeholder="Name of Prof."
+              value={profName}
               onChange={(e) => setProfName(e.target.value)}
-              className="block w-full rounded-md border-0 py-1.5 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
+              className="block w-full rounded-md border-0 py-1.5 pr-7 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
             />
+            {profName !== "" && (
+              <Button
+                type="button"
+                variant="ghost"
+                size="icon"
+                className="absolute right-1 top-1/2 -translate-y-1/2 h-7 w-7"
+                onClick={() => {
+                  setProfName("");
+                  document.getElementById("profName")?.focus();
+                }}
+              >
+                <XIcon className="h-4 w-4" />
+                <span className="sr-only">Clear</span>
+              </Button>
+            )}
           </div>
           <Button
             variant="secondary"
