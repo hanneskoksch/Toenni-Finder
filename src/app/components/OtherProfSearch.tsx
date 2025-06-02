d0b013e9c6a3a28e7d1c54d0f0f2cb9bdb057f15
Hannes Koksch <hannes.koksch@gmail.com>
2025-06-02 19:03:26 +0200

    fix: padding for text in seatch input field

--- src/app/components/OtherProfSearch.tsx
+++ src/app/components/OtherProfSearch.tsx
@@ -28,7 +28,7 @@ function OtherProfSearch({ onSearch }: OtherProfSearchProps) {
               placeholder="Name of Prof."
               value={profName}
               onChange={(e) => setProfName(e.target.value)}
-              className="block w-full rounded-md border-0 py-1.5 pr-7 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
+              className="block w-full rounded-md border-0 py-1.5 pl-2 pr-7 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
             />
             {profName !== "" && (
               <Button
