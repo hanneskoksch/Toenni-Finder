7bb3eb1656d4ecde97fe716eca181a59731f7939
Hannes Koksch <hannes.koksch@gmail.com>
2026-01-15 14:00:05 +0100

    refactor: replace html input with shdcn input

--- src/app/components/OtherProfSearch.tsx
+++ src/app/components/OtherProfSearch.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { XIcon } from "lucide-react";
-import { useState } from "react";
+import { useId, useRef, useState } from "react";
 
 interface OtherProfSearchProps {
   onSearch: (profName: string) => void;
@@ -14,7 +15,11 @@ function OtherProfSearch({ onSearch }: OtherProfSearchProps) {
     if (profName !== "") {
       onSearch(profName.trim());
       window.scrollTo(0, 0);
-      document.getElementById("profName")?.blur();
+
+      // unfocus input after search
+      if (inputRef.current) {
+        inputRef.current.blur();
+      }
     }
   };
 
@@ -24,51 +29,58 @@ function OtherProfSearch({ onSearch }: OtherProfSearchProps) {
     }
   };
 
+  const inputRef = useRef<HTMLInputElement>(null);
+  const id = useId();
+
+  const handleClearInput = () => {
+    setProfName("");
+
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  };
+
   return (
     <>
-      <div>
-        <label
-          htmlFor="profName"
-          className="block text-sm/6 font-medium text-gray-900 mb-3"
-        >
-          Looking for another Prof.?
-        </label>
-        <div className="flex space-x-3">
-          <div className="relative rounded-md shadow-xs ">
-            <input
-              id="profName"
-              name="profName"
-              type="text"
-              placeholder="Name of Prof."
-              value={profName}
-              onChange={(e) => setProfName(e.target.value)}
-              onKeyDown={handleKeyDown}
-              className="block w-full rounded-md border-0 py-1.5 pl-2 pr-7 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
-            />
-            {profName !== "" && (
-              <Button
-                type="button"
-                variant="ghost"
-                size="icon"
-                className="absolute right-1 top-1/2 -translate-y-1/2 h-7 w-7"
-                onClick={() => {
-                  setProfName("");
-                  document.getElementById("profName")?.focus();
-                }}
-              >
-                <XIcon className="h-4 w-4" />
-                <span className="sr-only">Clear</span>
-              </Button>
-            )}
-          </div>
-          <Button
-            variant="secondary"
-            onClick={handleSearch}
-            disabled={profName === ""}
-          >
-            Search
-          </Button>
+      <label
+        htmlFor={id}
+        className="block text-sm/6 font-medium text-gray-900 mb-3"
+      >
+        Looking for another prof?
+      </label>
+
+      <div className="flex gap-2">
+        <div className="relative">
+          <Input
+            name="profName"
+            placeholder="Name of Prof."
+            ref={inputRef}
+            id={id}
+            type="text"
+            value={profName}
+            onChange={(e) => setProfName(e.target.value)}
+            onKeyDown={handleKeyDown}
+            className="pr-9"
+          />
+          {profName && (
+            <Button
+              variant="ghost"
+              size="icon"
+              onClick={handleClearInput}
+              className="text-muted-foreground focus-visible:ring-ring/50 absolute inset-y-0 right-0 rounded-l-none hover:bg-transparent"
+            >
+              <XIcon className="h-4 w-4" />
+              <span className="sr-only">Clear input</span>
+            </Button>
+          )}
         </div>
+        <Button
+          variant="default"
+          onClick={handleSearch}
+          disabled={profName.trim() === ""}
+        >
+          Search
+        </Button>
       </div>
     </>
   );
